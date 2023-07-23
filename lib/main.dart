a97e5cbca0d783139b46e1b22fd91488abef4b0f
1STEP621 <onestep621@gmail.com>
2023-07-23 20:51:08 +0900

    タップするとその日にフォーカスするように&ロケール対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 void main() {
-  runApp(const MyApp());
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -46,6 +47,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // TODO
   }
 
+  DateTime? _selectedDay;
+  DateTime _focusedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,7 +67,17 @@ class _MyHomePageState extends State<MyHomePage> {
           TableCalendar(
             firstDay: DateTime.utc(2010, 1, 1),
             lastDay: DateTime.utc(9999, 12, 31),
-            focusedDay: DateTime.now(),
+            focusedDay: _focusedDay,
+            locale: 'ja_JP',
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay; // update `_focusedDay` here as well
+              });
+            },
           ),
         ],
       ),
