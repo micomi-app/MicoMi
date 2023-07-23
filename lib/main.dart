3c503925d022fa2da7beaec1f2275301c3f6a718
1STEP621 <onestep621@gmail.com>
2023-07-23 19:11:38 +0900

    ざっくりダークモード対応(要調整)

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MicoMi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blue,
+        ),
+        brightness: Brightness.light,
         useMaterial3: true,
       ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
       home: const MyHomePage(title: 'MicoMi'),
     );
   }
